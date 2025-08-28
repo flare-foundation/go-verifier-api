cf5a9aaf56ca45a3009fb0558785297f52dc12f1
jakobfir <jakob.fir@abelium.com>
2025-08-28 11:05:51 +0200

    Removed unnecessary test

--- internal/attestation/pmw_multisig_account/verifier_test.go
+++ internal/attestation/pmw_multisig_account/verifier_test.go
@@ -83,25 +83,3 @@ func TestSingleSig(t *testing.T) {
 	require.Equal(t, uint8(attestationtypes.PMWMultisigAccountStatusERROR), verify.Status)
 	require.Equal(t, uint64(0), verify.Sequence)
 }
-
-func TestBla(t *testing.T) {
-	service, err := NewMultisigService(envConfig)
-	require.NoError(t, err)
-
-	pubkey1, err := hexutil.Decode("0xED3F12A88266246B4D6E3886E9906E3C905F4378ACA1CDBACF7F6989CD940FE7F7")
-	require.NoError(t, err)
-
-	pubkey2, err := hexutil.Decode("0xED5E7464EF81CA10829CADE12BF0EF298A3BDCF66BF68600B9D5C47147DCE394A2")
-	require.NoError(t, err)
-
-	verifier := service.GetVerifier()
-	verify, err := verifier.Verify(t.Context(), connector.IPMWMultisigAccountConfiguredRequestBody{
-		WalletAddress: "rH29jBMdiGcyqPcPqS6d6qW1LZ9fMpV1aD",
-		PublicKeys:    [][]byte{pubkey1, pubkey2},
-		Threshold:     2,
-	})
-
-	require.NoError(t, err)
-	require.Equal(t, uint8(attestationtypes.PMWMultisigAccountStatusOK), verify.Status)
-	require.Equal(t, uint64(9882340), verify.Sequence)
-}
